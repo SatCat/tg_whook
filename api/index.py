9b672187e817626cc8d6f0fb6b672459faaa0416
SatCat <39083134+SatCat@users.noreply.github.com>
2024-06-28 20:52:51 +1100

    test .dev

--- api/index.py
+++ api/index.py
@@ -5,7 +5,6 @@ import httpx
 
 app = FastAPI()
 
-
 TARGET_HOST = os.environ.get('TARGET_HOST')
 headers = {'connection': 'keep-alive', 'cache-control': 'max-age=0', 'upgrade-insecure-requests': '1', 
            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0', 
